912c1bf9ab0e45bcb9e20946a0802e9e070457b3
Rane M Kaseman <raykman@comcast.net>
2018-08-17 23:01:44 -0600

    api diving

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -9,16 +9,16 @@ var from = "2105+Coronado+Pkwy+N,Denver,CO";
 var to = "1901+East+Asbury+Avenue,Denver,CO";
 
 var altRoutes = "http://www.mapquestapi.com/directions/v2/alternateroutes?key=" + apiKey + "&from=" + from + "&to=" + to + "&maxRoutes=2&timeOverage=100";
-console.log("altRoutes : " + altRoutes);
+// console.log("altRoutes : " + altRoutes);
 
 var optRoute = "http://www.mapquestapi.com/directions/v2/optimizedroute?key=" + apiKey + "&json={'locations':['Denver,CO','Westminster,CO','Boulder,CO']}";
-console.log("optRoute : " + optRoute);
+// console.log("optRoute : " + optRoute);
 
-var geocode = "http://www.mapquestapi.com/geocoding/v1/address?key=" + apiKey + "&location=" + to + "&thumbMaps=false";
+var geocode = "http://www.mapquestapi.com/geocoding/v1/address?key=" + apiKey + "&location=" + from + "&thumbMaps=false";
 console.log("geocode : " + geocode);
 
 var route = "https://www.mapquestapi.com/directions/v2/route?key=" + apiKey + "&from=" + from + "&to=" + to + "&outFormat=json&ambiguities=ignore&routeType=fastest&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false";
-console.log("route : " + route);
+// console.log("route : " + route);
 
 // var trafficDelays = "http://www.mapquestapi.com/traffic/v2/incidents?key=" + apiKey + "&boundingBox=39.95,-105.25,39.52,-104.71&filters=construction,incidents";
 
@@ -31,8 +31,9 @@ $.ajax({
     method: "GET"
 }) // end $.ajax
     .then(function (responseAr) {
-        console.log(responseAr);
-        // console.log(response.route);
+        // console.log(responseAr);
+        // console.log(responseAr.info);
+        // console.log(responseAr.route);
     }); //end .then
 
 $.ajax({
@@ -40,9 +41,8 @@ $.ajax({
     method: "GET"
 }) // end $.ajax
     .then(function (responseOr) {
-        console.log(responseOr);
-        // var print = JSON.stringify(responseOr);
-        // document.getElementById("test").innerHTML = JSON.stringify(responseOr);
+        // console.log(responseOr);
+        // console.log(responseOr);
     }); //end .then
 
 $.ajax({
@@ -51,7 +51,7 @@ $.ajax({
 }) // end $.ajax
     .then(function (responseGc) {
         console.log(responseGc);
-        // console.log(responseGc.info);
+        console.log(responseGc.results[0].locations);
     }); //end .then
 
 $.ajax({
@@ -59,7 +59,7 @@ $.ajax({
     method: "GET"
 }) // end $.ajax
     .then(function (response) {
-        console.log(response);
+        // console.log(response);
         // console.log(response.route);
     }); //end .then
 
@@ -82,5 +82,5 @@ $("#add-information-btn").on("click", function (event) {
     // console.log("arrival : " + arrival);
 });
 
-$("form")[0].reset();
+    $("form")[0].reset();
 
