236141b2c6fd12c63ff6f780658e75b434dc854d
Rane M Kaseman <raykman@comcast.net>
2018-08-18 20:13:54 -0600

    progress push

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -4,73 +4,87 @@ $(document).ready(function () {
 // accepted ajax GET address format:
 // 1555 Blake St, Denver, CO 80202
 
-var apiKey = "FX1GGwHVna3hSW5VqqYNR8FOjqQhDdlM";
+    var apiKey = "FX1GGwHVna3hSW5VqqYNR8FOjqQhDdlM";
 
-var deviceLatitude;
-var deviceLongitude;
-var from = "2105+Coronado+Pkwy+N,Denver,CO";
-var to = "1901+East+Asbury+Avenue,Denver,CO";
+    var deviceLatitude = 39.8335904;
+    var deviceLongitude = -104.95641979999999;
+    var origin = "2105+Coronado+Pkwy+N,Denver,CO";
+    var originStAddress = "2105+Coronado+Pkwy+N,Denver,CO";
+    var originCity = "2105+Coronado+Pkwy+N,Denver,CO";
+    var originState = "2105+Coronado+Pkwy+N,Denver,CO";
+    var originZipCode = "2105+Coronado+Pkwy+N,Denver,CO";
+    var dest = "1901+East+Asbury+Avenue,Denver,CO";
+    var destStAddress = "1901+East+Asbury+Avenue,Denver,CO";
+    var destCity = "1901+East+Asbury+Avenue,Denver,CO";
+    var destState = "1901+East+Asbury+Avenue,Denver,CO";
+    var destZipCode = "1901+East+Asbury+Avenue,Denver,CO";
 
-var altRoutes = "http://www.mapquestapi.com/directions/v2/alternateroutes?key=" + apiKey + "&from=" + from + "&to=" + to + "&maxRoutes=2&timeOverage=100";
-console.log("altRoutes : " + altRoutes);
+    var reverseGeocode = "http://www.mapquestapi.com/geocoding/v1/reverse?key=" + apiKey + "&location=" + deviceLatitude + "," + deviceLongitude + "&includeRoadMetadata=true&includeNearestIntersection=true";
+    console.log("reverseGeocode : " + reverseGeocode);
 
-var optRoute = "http://www.mapquestapi.com/directions/v2/optimizedroute?key=" + apiKey + "&json={'locations':['Denver,CO','Westminster,CO','Boulder,CO']}";
-console.log("optRoute : " + optRoute);
+    var route = "https://www.mapquestapi.com/directions/v2/route?key=" + apiKey + "&from=" + origin + "&to=" + dest + "&outFormat=json&ambiguities=ignore&routeType=fastest&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false";
+    console.log("route : " + route);
 
-var route = "https://www.mapquestapi.com/directions/v2/route?key=" + apiKey + "&from=" + from + "&to=" + to + "&outFormat=json&ambiguities=ignore&routeType=fastest&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false";
-console.log("route : " + route);
+    var altRoutes = "http://www.mapquestapi.com/directions/v2/alternateroutes?key=" + apiKey + "&from=" + origin + "&to=" + dest + "&maxRoutes=2&timeOverage=100";
+    // console.log("altRoutes : " + altRoutes);
 
-var reverseGeocode = "http://www.mapquestapi.com/geocoding/v1/reverse?key=" + apiKey + "&location=30.333472,-81.470448&includeRoadMetadata=true&includeNearestIntersection=true";
-console.log("reverseGeocode : " + reverseGeocode);
+    var optRoute = "http://www.mapquestapi.com/directions/v2/optimizedroute?key=" + apiKey + "&json={'locations':['Denver,CO','Westminster,CO','Boulder,CO']}";
+    // console.log("optRoute : " + optRoute);
 
-// var trafficDelays = "http://www.mapquestapi.com/traffic/v2/incidents?key=" + apiKey + "&boundingBox=39.95,-105.25,39.52,-104.71&filters=construction,incidents";
+    // var trafficDelays = "http://www.mapquestapi.com/traffic/v2/incidents?key=" + apiKey + "&boundingBox=39.95,-105.25,39.52,-104.71&filters=construction,incidents";
 
-// var fromStr = topicArr[i].split("+").join(" ");
+    // var fromStr = topicArr[i].split("+").join(" ");
 
 // if device geolocation exists populate current address
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition);
-    // else manual entry
+    // else warn and offer manual entry
     } else {
         $("#home_location_input").attr("placeholder", "Geolocation is not available on this device. Enter current address.");
     }
-
-function showPosition(position) {
-    $("#home_location_input").attr("placeholder", "Latitude: " + position.coords.latitude + " Longitude: " + position.coords.longitude);
+// grab device latitude and longitude
+function showPosition (position) {
     deviceLatitude = position.coords.latitude;
     deviceLongitude = position.coords.longitude;
+    $("#home_location_input").attr("placeholder", "Latitude: " + deviceLatitude + " Longitude: " + deviceLongitude);
     console.log(deviceLatitude);
     console.log(deviceLongitude);
 }
-// ^ geolocation ]
+// ^ device geolocation ]
 
 $.ajax({
-    url: altRoutes,
+    url: reverseGeocode,
     method: "GET"
-}) // end $.ajax
-    .then(function (responseAr) {
-        console.log(responseAr);
-        // console.log(responseAr.info);
-        // console.log(responseAr.route);
-    }); //end .then
-
-$.ajax({
-    url: optRoute,
-    method: "GET"
-}) // end $.ajax
-    .then(function (responseOr) {
-        // console.log(responseOr);
-        // console.log(responseOr);
-    }); //end .then
+}).then(function (response) {
+    geoLocation = response.results[0].locations[0].street + ", " + response.results[0].locations[0].adminArea5 + ", " + response.results[0].locations[0].adminArea3 + " " + response.results[0].locations[0].postalCode;
+    console.log(geoLocation);
+    console.log(response);
+});
 
 $.ajax({
     url: route,
     method: "GET"
-}) // end $.ajax
-    .then(function (response) {
-        // console.log(response);
-        // console.log(response.route);
-    }); //end .then
+}).then(function (response) {
+    // console.log(response);
+    // console.log(response.route);
+});
+
+$.ajax({
+    url: altRoutes,
+    method: "GET"
+}).then(function (response) {
+    // console.log(response);
+    // console.log(response.info);
+    // console.log(response.route);
+});
+
+$.ajax({
+    url: optRoute,
+    method: "GET"
+}).then(function (response) {
+    // console.log(response);
+    // console.log(response);
+});
 
 $("#add-information-btn").on("click", function (event) {
     console.log(event);
@@ -81,29 +95,15 @@ $("#add-information-btn").on("click", function (event) {
     destCity = $("#inputCity").val().trim();
     destState = $("#inputState").val().trim();
     destZip = $("#inputZip").val().trim();
-    // arrival = $("#arrival_time").val().trim();
     console.log("name : " + name);
     console.log("home : " + home);
     console.log("destination : " + destination);
     console.log("destCity : " + destCity);
     console.log("destState : " + destState);
     console.log("destZip : " + destZip);
-    // console.log("arrival : " + arrival);
 });
 
     $("form")[0].reset();
-
-// var geocode = "http://www.mapquestapi.com/geocoding/v1/address?key=" + apiKey + "&location=" + from + "&thumbMaps=false";
-// console.log("geocode : " + geocode);
-
-// $.ajax({
-    // url: geocode,
-    // method: "GET"
-// }) // end $.ajax
-    // .then(function (responseGc) {
-        // console.log(responseGc);
-        // console.log(responseGc.results[0].locations);
-    // }); //end .then
 
 });
 
