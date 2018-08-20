d64e4eede3f869f603f1d0cdb9b9b3833e6236f8
Rane M Kaseman <raykman@comcast.net>
2018-08-19 21:37:47 -0600

    progress push

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,42 +1,43 @@
 
-$(document).ready(function () {
+// $(document).ready(function () {
 
 // accepted ajax GET address format:
 // 1555 Blake St, Denver, CO 80202
 // 39.8427548 || 39.842736599999995
-// -104.96166260000001 || 104.9616483
+// -104.96166260000001 || -104.9616483
 
     var apiKey = "FX1GGwHVna3hSW5VqqYNR8FOjqQhDdlM";
 
-    var deviceLatitude = 39.8427;
-    var deviceLongitude = -104.9616;
-    console.log(deviceLatitude);
-    console.log(deviceLongitude);
-    var origin = "2105+Coronado+Pkwy+N,Denver,CO";
+    // var deviceLatitude = 39.8427;
+    // var deviceLongitude = -104.9616;
+    // console.log(deviceLatitude);
+    // console.log(deviceLongitude);
+    // var deviceLocSearchStr;
+    // console.log(deviceLocSearchStr);
+    var origin = "2105+Coronado+Pkwy+N,Denver,CO80229";
     var dest = "1901+East+Asbury+Avenue,Denver,CO";
 
-    var reverseGeocode = "http://www.mapquestapi.com/geocoding/v1/reverse?key=" + apiKey + "&location=" + deviceLatitude + "," + deviceLongitude + "&includeRoadMetadata=true&includeNearestIntersection=true";
-    console.log("reverseGeocode : " + reverseGeocode);
+    // var reverseGeocode = "http://www.mapquestapi.com/geocoding/v1/reverse?key=" + apiKey + "&location=" + deviceLatitude + "," + deviceLongitude + "&includeRoadMetadata=true&includeNearestIntersection=true";
+    // console.log("reverseGeocode : " + reverseGeocode);
 
-    var route = "https://www.mapquestapi.com/directions/v2/route?key=" + apiKey + "&from=" + origin + "&to=" + dest + "&outFormat=json&ambiguities=ignore&routeType=fastest&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false";
-    console.log("route : " + route);
+    // var route = "https://www.mapquestapi.com/directions/v2/route?key=" + apiKey + "&from=" + origin + "&to=" + dest + "&outFormat=json&ambiguities=ignore&routeType=fastest&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false";
+    // console.log("route : " + route);
 
-    var routePed = "https://www.mapquestapi.com/directions/v2/route?key=" + apiKey + "&from=" + origin + "&to=" + dest + "&outFormat=json&ambiguities=ignore&routeType=pedestrian&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false";
-    console.log("routePed : " + routePed);
-
-    // var fromStr = topicArr[i].split("+").join(" ");
-
-
+    // var routePed = "https://www.mapquestapi.com/directions/v2/route?key=" + apiKey + "&from=" + origin + "&to=" + dest + "&outFormat=json&ambiguities=ignore&routeType=pedestrian&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false";
+    // console.log("routePed : " + routePed);
 
     // if device geolocation exists
     if (navigator.geolocation) {
         // grab device latitude and longitude
         navigator.geolocation.getCurrentPosition(function (position) {
+        // navigator.geolocation.watchPosition(function (position) {
             deviceLatitude = position.coords.latitude;
             deviceLongitude = position.coords.longitude;
             console.log(deviceLatitude);
             console.log(deviceLongitude);
+            if (deviceLatitude && deviceLongitude) {
             doAjax()
+            };
         });
         // else warn and offer manual entry
         }
@@ -45,21 +46,23 @@ $(document).ready(function () {
         };
     // ^ device geolocation
 
-
-
     function doAjax() {
-        if (deviceLatitude && deviceLongitude) {
             $.ajax({
-                url: reverseGeocode,
+                url: "http://www.mapquestapi.com/geocoding/v1/reverse?key=" + apiKey + "&location=" + deviceLatitude + "," + deviceLongitude + "&includeRoadMetadata=true&includeNearestIntersection=true",
                 method: "GET"
             }).then(function (response) {
                 deviceLocation = response.results[0].locations[0].street + "," + response.results[0].locations[0].adminArea5 + "," + response.results[0].locations[0].adminArea3 + response.results[0].locations[0].postalCode;
-                $("#home_location_input").val(deviceLocation);
-                console.log("deviceLocation : " + deviceLocation);
+                $("#start_address_input").val(response.results[0].locations[0].street);
+                $("#start_city_input").val(response.results[0].locations[0].adminArea5);
+                $("#start_state_input").val(response.results[0].locations[0].adminArea3);
+                $("#start_zip_input").val(response.results[0].locations[0].postalCode);
+                deviceLocSearchStr = deviceLocation.replace(/\s/g, "+");
+                console.log("deviceLocSearchStr : " + deviceLocSearchStr);
+                return(deviceLocSearchStr);
             });
 
             $.ajax({
-                url: route,
+                url: "https://www.mapquestapi.com/directions/v2/route?key=" + apiKey + "&from=" + origin + "&to=" + dest + "&outFormat=json&ambiguities=ignore&routeType=fastest&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false",
                 method: "GET"
             }).then(function (response) {
                 timeEstimate = response.route.legs[0].formattedTime;
@@ -67,25 +70,23 @@ $(document).ready(function () {
             });
 
             $.ajax({
-                url: routePed,
+                url: "https://www.mapquestapi.com/directions/v2/route?key=" + apiKey + "&from=" + origin + "&to=" + dest + "&outFormat=json&ambiguities=ignore&routeType=pedestrian&doReverseGeocode=false&enhancedNarrative=false&avoidTimedConditions=false",
                 method: "GET"
             }).then(function (response) {
                 timeEstimatePed = response.route.legs[0].formattedTime;
                 console.log("timeEstimatePed : " + timeEstimatePed);
             });
-        };
+
     };
 
-
-
-    $("#add-information-btn").on("click", function (event) {
+    $("#add-info").on("click", function (event) {
         event.preventDefault();
         name = $("#name_input").val().trim();
-        home = $("#home_location_input").val().trim();
-        destination = $("#destination_input").val().trim();
-        destCity = $("#inputCity").val().trim();
-        destState = $("#inputState").val().trim();
-        destZip = $("#inputZip").val().trim();
+        home = $("#start_address_input").val().trim();
+        destination = $("#end_address_input").val().trim();
+        destCity = $("#end_city_input").val().trim();
+        destState = $("#end_state_input").val().trim();
+        destZip = $("#end_zip_input").val().trim();
         console.log("name : " + name);
         console.log("home : " + home);
         console.log("destination : " + destination);
@@ -95,11 +96,7 @@ $(document).ready(function () {
         console.log(event);
     });
 
-
-
         $("form")[0].reset();
-
-
 
     // function callMeWhenDone() {
     //     console.log("finished");
@@ -121,5 +118,5 @@ $(document).ready(function () {
     //     return a * b;            // Function returns the product of a and b
     // }
 
-});
+// });
 
